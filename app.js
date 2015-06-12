68c652d78b460f312d09ca217aa366015d69946a
lxchavez <Alex.Chavez@longbeach.gov>
2015-06-12 21:26:59 +0100

    Contacts can now be updated and edited more than once; this issue was caused by the additional tags and lastModifiedDate on the form. The parseForm() function did not like this

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ if (Meteor.isClient) {
     var formData = new Object();
     formData = contact;
     for (var prop in formData) {
-      if (formData.hasOwnProperty(prop)) {
+      if (formData.hasOwnProperty(prop) && (prop !== "tags" && prop != "lastModifiedDate")) {
         formData[prop] = e.target[prop].value;
       }
     }
@@ -124,45 +124,38 @@ if (Meteor.isClient) {
   Template.AddContact.events({
     "submit .new-contact": function (event, template) {
       event.preventDefault();
-
+      event.stopPropagation();
       // Get submitted form data and sanitize it for proper entry and retrieval
       var form = parseForm(event);
       form = sanitizeContactFormData(form);
-
       // Only insert a record if there was an entered name
       var name = form["name"].trim();
       if (name !== "" && typeof name === "string") {
         form["lastModifiedDate"] = new Date();
         Contacts.insert(form);
       }
-
       $(".new-contact").parsley().reset();
       template.find("form").reset(); // reset form
       $("#addContactModal").modal("hide");
-
-      return false; // prevent default form submit
+      return false;
     }
   });
 
   Template.EditContact.events({
     "submit .edit-contact": function (event, template) {
       event.preventDefault();
-
       // Get submitted form data and sanitize it for proper update and retrieval
       var id = Session.get("editContactId");
       var form = parseForm(event);
       form = sanitizeContactFormData(form);
-
       // Only insert a record if there was an entered name
       var name = form["name"].trim();
       if (name !== "" && typeof name === "string") {
         form["lastModifiedDate"] = new Date();
         Contacts.update({_id: id}, { $set: form });
       }
-
       $(".edit-contact").parsley().reset();
       $("#editContactModal").modal("hide");
-
       return false;
     },
     "click .delete-contact-btn": function (event) {
