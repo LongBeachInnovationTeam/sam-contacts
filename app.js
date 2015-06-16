cfc01be2552615bf4c65b9db5784f2644aceb25e
Alex Chavez <alex@Alexs-MacBook-Pro.local>
2015-06-16 16:14:25 -0700

    Starting to add the "add interaction" feature

--- app.js
+++ app.js
@@ -126,6 +126,11 @@ if (Meteor.isClient) {
   });
 
   Template.ContactsList.events({
+    "click #add-interaction-btn": function (event) {
+      var id = event.target.dataset.id;
+      Session.set("editContactId", id);
+      $("#add-interaction-modal").modal("show");
+    },
     "click .contact-edit-btn": function (event) {
       var id = event.target.id;
       Session.set("editContactId", id);
@@ -170,7 +175,8 @@ if (Meteor.isClient) {
         tags: sanitizeTags(tags) || [],
         regularMeetings: regularMeetings,
         notableAnnualEvents: notableAnnualEvents,
-        notes: notes
+        notes: notes,
+        interactions: new Array()
       }
 
       var isValidName = newContact.name !== "" && newContact.name;
@@ -324,6 +330,56 @@ if (Meteor.isClient) {
       return;
     });
   }
+
+  var resetAddInteractionForm = function (template) {
+    // Reset form, hide modal, and return to caller
+    $(".add-interaction").parsley().reset();
+    template.find(".add-interaction").reset();
+    $("#add-interaction-modal").modal("hide");
+  }
+
+  Template.AddInteraction.helpers({
+    contactInfo: function () {
+      var id = Session.get("editContactId");
+      return Contacts.findOne({_id: id});
+    }
+  });
+
+  Template.AddInteraction.events({
+    "submit .add-interaction": function (event, template) {
+      event.preventDefault();
+
+      var id = Session.get("editContactId");
+      var interactionDate = $("#add-interaction-date-field").val();
+      var estimatedAttendees = $("#add-interaction-estimated-attendees-field").val();
+      var purpose = $("#add-interaction-purpose-field").val();
+      var outcome = $("#add-interaction-outcome-field").val();
+      var followUpAction = $("#add-interaction-follow-up-field").val();
+      var notes = $("#add-interaction-notes-field").val();
+
+      var interaction = {
+        interactionDate: interactionDate,
+        estimatedAttendees: estimatedAttendees,
+        purpose: purpose,
+        outcome: outcome,
+        followUpAction: followUpAction,
+        notes: notes
+      };
+
+      var isValidId = id !== "" && id;
+      var isValidInteractionDate = interaction.interactionDate !== "" && interaction.interactionDate;
+
+      if (isValidId && isValidInteractionDate) {
+        var editedContact = Contacts.findOne({_id: id});
+        editedContact.interactions.push(interaction);
+        editedContact.lastModifiedDate = new Date();
+        Contacts.update({_id: id}, { $set: editedContact });
+      }
+
+      resetAddInteractionForm(template);
+      return false;
+    }
+  });
 
 }
 
