ac95fbe0b8ab7258b5052d88b75e9a6edabc0ad9
Alex Chavez <alex@alexchavez.net>
2015-07-16 21:15:59 -0700

    Clean up

--- client/views/juicy_quotes.js
+++ client/views/juicy_quotes.js
@@ -1,26 +1,11 @@
 if (Meteor.isClient) {
 
-  // incrementLimit = function (inc) {
-  //   if (!inc) {
-  //     inc = 10;
-  //   }
-  //   var newLimit = Session.get("limit") + inc;
-  //   Session.set("limit", newLimit);
-  // }
-
   var colorCode = 0;
-
-  var resetAddQuoteForm = function () {
-    // Reset form, hide modal, and return to caller
-    $(".new-contact").parsley().reset();
-    $(".new-contact")[0].reset();
-    $("#add-quote-modal").modal("hide");
-  }
 
   Template.JuicyQuotes.helpers({
     contacts: function () {
       return Contacts.find({}, {
-        limit: Session.get("limit")
+        limit: 50
       });
     },
     getQuoteType: function () {
@@ -31,23 +16,5 @@ if (Meteor.isClient) {
       return "quoute-type" + colorCode;
     }
   });
-
-  Template.JuicyQuotes.created = function () {
-    Session.setDefault("limit", 50);
-    // Deps.autorun() automatically rerun the subscription whenever Session.get('limit') changes
-    // http://docs.meteor.com/#deps_autorun
-    Deps.autorun(function() {
-      Meteor.subscribe("getContacts", Session.get("limit"));
-    });
-  }
-
-  // Template.JuicyQuotes.rendered = function () {
-  //   // is triggered every time we scroll
-  //   $(window).scroll(function() {
-  //     if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-  //       incrementLimit();
-  //     }
-  //   });
-  // }
 
 }
