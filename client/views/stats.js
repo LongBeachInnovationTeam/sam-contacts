306d526b814ffc7659fc33ee318a4228ece25c08
Alex Chavez <alex@alexchavez.net>
2015-08-25 11:36:51 -0700

    Added stats page/route; cleaned up contact page

---
+++ client/views/stats.js
@@ -0,0 +1,19 @@
+if (Meteor.isClient) {
+
+	Template.Stats.helpers({
+
+	});
+
+	Template.Stats.events({
+
+	});
+
+	Template.Stats.created = function () {
+
+	}
+
+	Template.Stats.rendered = function () {
+
+	}
+
+}
